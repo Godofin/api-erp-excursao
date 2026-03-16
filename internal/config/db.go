4c6ae95cc56ebfa7cf73929a2722a5cc903c0baa
Guilherme Lavezzo <65193596+Godofin@users.noreply.github.com>
2026-03-16 20:28:19 -0300

    Update db.go

--- internal/config/db.go
+++ internal/config/db.go
@@ -19,7 +19,7 @@ func InitDB() {
 	dbname := os.Getenv("DB_NAME")
 	port := os.Getenv("DB_PORT")
 
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable", host, user, password, dbname, port)
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=require", host, user, password, dbname, port)
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal("Falha ao conectar ao banco de dados:", err)
